da883a871e1cfbdddb65f2deaadf3a99b4db8d3d
VarArm <1162397414@qq.com>
2019-07-13 10:05:25 +0800

    one

---
+++ mympvue/src/pages/item/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>item</div>
+</template>
+
+<script>
+import heroitem from "../../utils/lol_details_duowan.js";
+export default {
+  data() {
+    return {
+      heroitem: heroitem,
+      heroObj: {}
+    };
+  },
+  mounted() {
+    let id = this.$root.$mp.query.id;
+    console.log(id);
+    console.log(this.heroitem);
+
+    this.heroObj = this.heroitem.filter(item => {
+      return item.id === id;
+    })[0];
+    // this.heroObj = this.heroitem.filter(item => {
+    //   return item.id === id;
+    // })[0];
+    console.log(this.heroObj);
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ mympvue/src/pages/list/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mybox">
+    <div class="list" v-for="(item,index) in datalist" :key="index">
+      <a :href="'/pages/item/main?id='+item.id">
+        <img :src="item.iconUrl" alt />
+        <p>{{item.name}}</p>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import datalist from "../../utils/lol_duowan";
+export default {
+  data() {
+    return {
+      datalist: datalist
+    };
+  }
+};
+</script>
+
+<style scoped lang='less'>
+.mybox {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.list {
+  width: 33.33%;
+  text-align: center;
+  a {
+    width: 100%;
+    height: 100%;
+  }
+  img {
+    width: 100px;
+    height: 100px;
+  }
+}
+</style>
